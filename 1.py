5429b673b3ccb17e1371828618d851df1d68df32
07Roshi <roshithshetty7@gmail.com>
2024-02-26 19:17:36 +0530

    changes

--- 1.py
+++ 1.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-    sequence = [0, 1]
-    while len(sequence) < n:
-        sequence.append(sequence[-1] + sequence[-2])
-    return sequence
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-print(fibonacci(10))
+num = int(input("Enter a number: "))
+print("The factorial of", num, "is", factorial(num))
